d69741b4412b36967997425cba556e39a9293e13
Daniel Martínez Cruz <danilinu@hotmail.com>
2012-12-20 19:22:29 +0100

    Prueba 1

--- presentacio/menuSuperior.php
+++ presentacio/menuSuperior.php
@@ -3,7 +3,7 @@
 						<a class="linkMenu1"		title="Servicios"		href="servicios.php"		>Simulació Foc</a>
 						<!--<a class="linkMenu1"		title="Mobiliario"	href="mobiliario.php"	>Mobiliario</a>
 						<a class="linkMenu1"		title="Orfebreria" 	href="orfebreria.php"	>Orfebreria</a>-->
-						<a class="linkMenu1"		title="Promociones"	href="promociones.php"	>Sumlació Caiguda</a>
+						<a class="linkMenu1"		title="Simulació Caiguda"	href=""	>Sumlació Caiguda</a>
 						<a class="linkMenu1"		title="Catálogo" 	href="catalogo.php"	>Simulació Tardança</a>
 						<!-- <a class="linkMenu1"		title="Libros" 		href="libros.php"		>Libros</a>
 						<a class="linkMenu1"		title="Varios" 		href="varios.php"		>Varios</a> -->
