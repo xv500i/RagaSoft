ce0860b0a105720eed125e1d0e01e57e9eec39cf
Alex <xv500i@gmail.com>
2012-12-26 10:59:44 +0100

    fabrica de dades

---
+++ dades/FabricaControladorsDades.php
@@ -0,0 +1,12 @@
+<?php
+class SimpleClass
+{
+    // Declaración de la propiedad
+    public $var = 'a default value';
+
+    // Declaración del método
+    public function displayVar() {
+        echo $this->var;
+    }
+}
+?>
