6c8ace2b71ad51090d4e9667160d26abecc7de62
David Gràcia i Llobet <gruay.dgll@gmail.com>
2013-01-03 13:32:08 +0100

    Getters i setters de Cuidador

--- domini/Cuidador.php
+++ domini/Cuidador.php
@@ -8,6 +8,18 @@ class Cuidador {
 	public function obteTelefon() {
 		return $telefon;
 	}
+	
+	public function modificaTelefon($t) {
+		$telefon = $t;
+	}
+	
+	public function obteNom() {
+		return $nom;
+	}
+	
+	public function modificaNom($n) {
+		$nom = $n;
+	}
 }
 
 ?>
