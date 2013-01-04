4109fd87c80651ba9c01e3eed48701085717d330
Ruben <rferrebaiges@gmail.com>
2013-01-04 12:55:20 +0100

    proba

--- domini/proves.php
+++ domini/proves.php
@@ -3,9 +3,17 @@
 	include_once ("TxCreaCaiguda.php");
 	include_once ("TxCreaTardanca.php");
 	
+
+	
+	echo "Crear Incendi";
+	echo "<br>";
+	$TxIn = new TxCreaIncedi();
+	$TxIn->execu();
+	$i = $TxIn->obteResultat();
+	
 	echo "Notifica";
 	echo "<br>";
-	
 	$tr = new TxNotifica();
+	$tr->modificaEmergencia($i);
 	$tr->execu();
 ?>
