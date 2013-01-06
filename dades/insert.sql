13c6ee76319569cf960f24c3ce3370ffcbcdd87e
Alex <xv500i@gmail.com>
2013-01-06 12:25:15 +0100

    inserts amb més d'un cuidador per casa

--- dades/insert.sql
+++ dades/insert.sql
@@ -4,3 +4,4 @@ INSERT INTO `test`.`resident` (`horaArribada`, `idRfid`, `nom`, `teCaigudaActiva
 INSERT INTO `test`.`cuidador` (`telefon`, `nom`, `usuariLlar`) VALUES ('696396666', 'Alex Soms Batalla', 'CasaPuig'), ('633768939', 'Carles Gonzalez Pimas', 'PisBarriAntic');
 INSERT INTO `test`.`emergencia` (`moment`, `tipus`, `usuariLlar`, `idRfidResident`) VALUES ('2012-12-31 23:59:59', 'Incendi', 'CasaPuig', NULL), ('2013-01-01 08:35:21', 'Caiguda', NULL, '1'), ('2012-12-28 13:00:00', 'Tardanca', NULL, '3');
 INSERT INTO `test`.`notificacio` (`id`, `idCuidador`, `momentEmergencia`, `confirmada`, `esPotConfirmar`) VALUES (NULL, '696396666', '2012-12-31 23:59:59', '1', '0'), (NULL, '696396666', '2013-01-01 08:35:21', '1', '0'), (NULL, '633768939', '2012-12-28 13:00:00', '1', '0');
+INSERT INTO `test`.`cuidador` (`telefon`, `nom`, `usuariLlar`) VALUES ('639564565', 'Joel Slenderman', 'CasaPuig'), ('698543515', 'Daniela Fernandez Espinola', 'CasaPuig');
