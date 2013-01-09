633f803de0c1b9fd20c9137f9750da5fd2a930ff
David Gràcia i Llobet <gruay.dgll@gmail.com>
2013-01-09 11:08:08 +0100

    asdfasdfsda

--- presentacio/procedimiento.php
+++ presentacio/procedimiento.php
@@ -1,7 +1,7 @@
 <?php 
 
 echo "holaoeu";
-echo exec("php " . __DIR__ . "enviasmsalos2minutos.php &", $r);
+echo exec("php " . __DIR__ . DIRECTORY_SEPARATOR."enviasmsalos2minutos.php &", $r);
 //include ("enviasmsalos2minutos.php");
 echo "<br>" . var_dump($r);
 //echo exec("whoami");
