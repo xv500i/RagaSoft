5a8ac8ff85ceaba9cca152e53274e51acd43715c
David Gràcia i Llobet <gruay.dgll@gmail.com>
2013-01-09 22:48:16 +0100

    lo del botó

--- presentacio/procedimiento.php
+++ presentacio/procedimiento.php
@@ -1,17 +1,17 @@
 <?php 
 
 echo "holaoeu";
-//echo exec("php " . __DIR__ . DIRECTORY_SEPARATOR."enviasmsalos2minutos.php &", $r);
+echo exec("php " . __DIR__ . DIRECTORY_SEPARATOR."enviasmsalos2minutos.php &", $r);
 //include ("enviasmsalos2minutos.php");
-//echo "<br>" . var_dump($r);
+echo "<br>" . var_dump($r);
 //echo exec("whoami");
-$r = new HttpRequest('http://localhost/rfid/presentacio/enviasmsalos2minutos.php', HttpRequest);
-echo "blaa";
-try {
-    echo "<br>" . $r->send();
-} catch (HttpException $ex) {
-    echo $ex;
-}
+//$r = new HttpRequest('http://localhost/rfid/presentacio/enviasmsalos2minutos.php', HttpRequest);
+//echo "blaa";
+//try {
+//    echo "<br>" . $r->send();
+//} catch (HttpException $ex) {
+//    echo $ex;
+//}
 
 echo "<br>terminado";
 ?>
