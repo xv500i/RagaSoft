37227d1ddebf6a62b2bdb1c509eafa40c65da209
David Gràcia i Llobet <gruay.dgll@gmail.com>
2013-01-09 16:53:13 +0100

    Els meus intents
    He intentat fer lo de la petició GET

--- presentacio/procedimiento.php
+++ presentacio/procedimiento.php
@@ -1,9 +1,17 @@
 <?php 
 
 echo "holaoeu";
-echo exec("php " . __DIR__ . DIRECTORY_SEPARATOR."enviasmsalos2minutos.php &", $r);
+//echo exec("php " . __DIR__ . DIRECTORY_SEPARATOR."enviasmsalos2minutos.php &", $r);
 //include ("enviasmsalos2minutos.php");
-echo "<br>" . var_dump($r);
+//echo "<br>" . var_dump($r);
 //echo exec("whoami");
+$r = new HttpRequest('http://kelbert.es/RagaSoft/presentacio/enviasmsalos2minutos.php', HttpRequest::METH_GET);
+echo "blaa";
+try {
+    echo "<br>" . $r->send();
+} catch (HttpException $ex) {
+    echo $ex;
+}
+
 echo "<br>terminado";
 ?>
